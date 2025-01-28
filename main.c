aaab6eab4aed5d66261fd4bd12b6c283a50b8284
ali.m <ali.masooman@gmail.com>
2025-01-28 14:39:12 +0330

    adding login menu

--- main.c
+++ main.c
@@ -2,6 +2,7 @@
 #include <stdio.h>
 #include <string.h>
 #include "sign_up.c"
+#include "login.c"
 
 int signup_login_menu()
 {
@@ -14,13 +15,11 @@ int signup_login_menu()
 
     // Initialize variables
     int choice = 0;
-    const char *options[] = {"Login", "Sign Up"};
+    const char *options[] = {"Login", "Sign Up", "Login as Guest"};
     const int num_options = sizeof(options) / sizeof(options[0]);
 
     while (1) {
-        //clear();
-
-        // Print menu options
+        clear();
         for (int i = 0; i < num_options; i++) {
             if (i == choice) {
                 attron(A_REVERSE); 
@@ -33,7 +32,7 @@ int signup_login_menu()
 
         int input = getch(); 
 
-        // Handle user input
+
         if (input == 'w' || input == KEY_UP || input == 'W') 
         {
             choice = (choice - 1 + num_options) % num_options; // Move up
@@ -57,12 +56,16 @@ int main()
     int choise = signup_login_menu();
     if(choise == 0)
     {
-        
+        login_page();
     }
     else if(choise == 1)
     {
         sign_up_page();
         main();
+    }
+    else if(choise == 2)
+    {
+        
     }
     return 0;
 }
